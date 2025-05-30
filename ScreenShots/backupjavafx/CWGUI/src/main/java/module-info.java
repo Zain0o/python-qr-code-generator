0869509319fac468b58fdb92b7683ca348ed2dd7
Zain0o <149389592+Zain0o@users.noreply.github.com>
2025-05-30 12:02:26 +0100

    Delete ScreenShots/backupjavafx directory

--- ScreenShots/backupjavafx/CWGUI/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module CWGUI {
-    requires javafx.controls;
-    requires javafx.graphics;
-    requires transitive javafx.base;
-    requires org.json;
-
-    exports GUI;
-    exports RobotSim;
-}
